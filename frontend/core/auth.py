edb33b3fbed70071b0aa555dcaae99014abc610f
munyajunior <ivojunior671@gmail.com>
2025-02-22 12:17:44 +0100

    Update auth.py
    authenticate user with backend, retrieve auth token, user_id and role, store for rba to manage other crude operations

--- frontend/core/auth.py
+++ frontend/core/auth.py
@@ -1,24 +1,34 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class AuthHandler:
     def __init__(self):
-        self.api_url = os.getenv("LOGIN_URL", "http://127.0.0.1:8000/api/auth/login")
+        self.api_url = os.getenv("LOGIN_URL")
 
     def authenticate(self, email, password):
         data = {"email": email, "password": password}
         try:
             response = requests.post(self.api_url, json=data)
+            
             if response.status_code == 200:
                 result = response.json()
                 token = result.get("access_token")
                 role = result.get("role")
-                self.save_token(token)
+                user_id = str(result.get("sub"))
+                
+                self.save_token(token, role, user_id)
                 return True, "Login successful!", role
             return False, "Invalid credentials. Please try again.", None
         except requests.exceptions.RequestException:
             return False, "Unable to connect to the server.", None
 
-    def save_token(self, token):
+    def save_token(self, token, role, user_id):
         with open("auth_token.txt", "w") as file:
             file.write(token)
+        with open("role.txt", "w") as file:  
+            file.write(role)
+        with open("user_id.txt", "w") as file:  
+            file.write(user_id)
