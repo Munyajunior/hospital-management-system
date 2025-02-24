eccbdd03ff40917a5e4676e17af403745b8089da
munyajunior <ivojunior671@gmail.com>
2025-02-24 03:44:29 +0100

    Update api_utils.py

--- frontend/utils/api_utils.py
+++ frontend/utils/api_utils.py
@@ -2,7 +2,7 @@ import os
 import requests
 from PySide6.QtWidgets import QMessageBox
 
-def fetch_data(api_url, auth_token=None, params=None):
+def fetch_data(self, api_url, auth_token=None, params=None):
     """Generic function to fetch data from an API."""
     headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
     try:
@@ -10,13 +10,13 @@ def fetch_data(api_url, auth_token=None, params=None):
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 403:
-                QMessageBox.critical("Error", "Access forbidden. You don't have permission.")
+                QMessageBox.critical(self,"Error", "Access forbidden. You don't have permission.")
         elif response.status_code == 401:
-                QMessageBox.critical("Error", "Unauthorized. Please log in again.")
+                QMessageBox.critical(self,"Error", "Unauthorized. Please log in again.")
         else:
-                QMessageBox.critical("Error", f"Failed to fetch data. Error {response.status_code}")
+                QMessageBox.critical(self,"Error", f"Failed to fetch data. Error {response.status_code}")
     except Exception as e:
-        QMessageBox.critical(None, "Error", f"An error occurred: {e}")
+        QMessageBox.critical(self, None, "Error", f"An error occurred: {e}")
         return None
 
 def post_data(api_url, data, auth_token=None):
@@ -29,6 +29,19 @@ def post_data(api_url, data, auth_token=None):
             return True
         else:
             QMessageBox.critical(None, "Error", f"Failed to post data to {api_url}")
+            return False
+    except Exception as e:
+        QMessageBox.critical(None, "Error", f"An error occurred: {e}")
+        return False
+
+def update_data(api_url, data, auth_token):
+    headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
+    try:
+        response = requests.put(api_url, json=data, headers=headers)
+        if response.status_code in [200, 201]:
+            return True
+        else:
+            QMessageBox.critical(None, "Error", f"Failed to update data to {api_url}")
             return False
     except Exception as e:
         QMessageBox.critical(None, "Error", f"An error occurred: {e}")
