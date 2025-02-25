6a81e106a5697e88e02f8e970672a8971a8003df
munyajunior <ivojunior671@gmail.com>
2025-02-25 21:22:22 +0100

    Update doctor.py

--- backend/app/models/doctor.py
+++ backend/app/models/doctor.py
@@ -11,10 +11,8 @@ class Doctor(Base):
     contact_number = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True, nullable=False) # Foreign Key to User
+
     # Relationships
     patients = relationship("Patient", back_populates="assigned_doctor")
-    
-    # Foreign Key to User
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True, nullable=False)
     user = relationship("User", back_populates="doctor")
