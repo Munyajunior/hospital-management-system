aab8af45d9f8d06342234d3bbf65164529dbb736
munyajunior <ivojunior671@gmail.com>
2025-02-25 21:23:46 +0100

    Update security.py

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -14,7 +14,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Secret key for JWT
 SECRET_KEY:str = os.getenv("SECRET_KEY")
 ALGORITHM :str= os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES :int = 30  # Recommended expiry time
+ACCESS_TOKEN_EXPIRE  = int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS"))# Recommended expiry time
 
 # Hash password
 def hash_password(password: str) -> str:
@@ -34,7 +34,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
