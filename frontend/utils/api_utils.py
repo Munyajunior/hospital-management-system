873d912b734cf09da7b75e18e944a6a95ed6741a
munyajunior <ivojunior671@gmail.com>
2025-02-25 21:23:31 +0100

    Update api_utils.py

--- frontend/utils/api_utils.py
+++ frontend/utils/api_utils.py
@@ -19,7 +19,7 @@ def fetch_data(self, api_url, auth_token=None, params=None):
         QMessageBox.critical(self, None, "Error", f"An error occurred: {e}")
         return None
 
-def post_data(api_url, data, auth_token=None):
+def post_data(self, api_url, data, auth_token=None):
     """Generic function to post data to an API."""
     
     headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
@@ -27,22 +27,30 @@ def post_data(api_url, data, auth_token=None):
         response = requests.post(api_url, json=data, headers=headers)
         if response.status_code in [200, 201]:
             return True
+        elif response.status_code == 403:
+                QMessageBox.critical(self,"Error", "Access forbidden. You don't have permission.")
+        elif response.status_code == 401:
+                QMessageBox.critical(self,"Error", "Unauthorized. Please log in again.")
         else:
-            QMessageBox.critical(None, "Error", f"Failed to post data to {api_url}")
+            QMessageBox.critical(self, "Error", f"Failed to post data to {api_url}")
             return False
     except Exception as e:
-        QMessageBox.critical(None, "Error", f"An error occurred: {e}")
+        QMessageBox.critical(self, "Error", f"An error occurred: {e}")
         return False
 
-def update_data(api_url, data, auth_token):
+def update_data(self, api_url, data, auth_token):
     headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
     try:
         response = requests.put(api_url, json=data, headers=headers)
         if response.status_code in [200, 201]:
             return True
+        elif response.status_code == 403:
+                QMessageBox.critical(self,"Error", "Access forbidden. You don't have permission.")
+        elif response.status_code == 401:
+                QMessageBox.critical(self,"Error", "Unauthorized. Please log in again.")
         else:
-            QMessageBox.critical(None, "Error", f"Failed to update data to {api_url}")
+            QMessageBox.critical(self, "Error", f"Failed to update data to {api_url}")
             return False
     except Exception as e:
-        QMessageBox.critical(None, "Error", f"An error occurred: {e}")
+        QMessageBox.critical(self, "Error", f"An error occurred: {e}")
         return False
