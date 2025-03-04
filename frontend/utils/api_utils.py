388fad31fbdd5dc7bebf04ae5869da55ccc4db80
munyajunior <ivojunior671@gmail.com>
2025-03-04 07:53:28 +0100

    Update api_utils.py

--- frontend/utils/api_utils.py
+++ frontend/utils/api_utils.py
@@ -16,7 +16,7 @@ def fetch_data(self, api_url, auth_token=None, params=None):
         else:
                 QMessageBox.critical(self,"Error", f"Failed to fetch data. Error {response.status_code}")
     except Exception as e:
-        QMessageBox.critical(self, None, "Error", f"An error occurred: {e}")
+        QMessageBox.critical(self, "Error", f"An error occurred: {e}")
         return None
 
 def post_data(self, api_url, data, auth_token=None):
@@ -38,7 +38,7 @@ def post_data(self, api_url, data, auth_token=None):
         QMessageBox.critical(self, "Error", f"An error occurred: {e}")
         return False
 
-def update_data(self, api_url, data, auth_token):
+def update_data(self, api_url, data, auth_token=None):
     headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
     try:
         response = requests.put(api_url, json=data, headers=headers)
@@ -54,3 +54,22 @@ def update_data(self, api_url, data, auth_token):
     except Exception as e:
         QMessageBox.critical(self, "Error", f"An error occurred: {e}")
         return False
+    
+
+def delete_data(self, api_url, auth_token=None, params=None):
+    headers = {"Authorization": f"Bearer {auth_token}"} if auth_token else {}
+    try:
+        response = requests.delete(api_url, headers=headers, params=params)
+        if response.status_code in [200, 201]:
+            return True
+        elif response.status_code == 403:
+                QMessageBox.critical(self,"Error", "Access forbidden. You don't have permission.")
+        elif response.status_code == 401:
+                QMessageBox.critical(self,"Error", "Unauthorized. Please log in again.")
+        else:
+            QMessageBox.critical(self, "Error", f"Failed to delete data {api_url}")
+            return False
+    except Exception as e:
+        QMessageBox.critical(self, "Error", f"An error occurred: {e}")
+        return False
+    
