f54bba2718ddfb6996697bad9d0fb86fde11fd5c
munyajunior <ivojunior671@gmail.com>
2025-03-04 07:48:49 +0100

    Update icu.py
    updated timestamp to be managed by database

--- backend/app/models/icu.py
+++ backend/app/models/icu.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, Enum
+from sqlalchemy.sql import func
 from enum import Enum as PyEnum
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from core.database import Base
 
 class ICUStatus(str, PyEnum):
@@ -16,7 +16,7 @@ class ICUPatient(Base):
     id = Column(Integer, primary_key=True, index=True)
     patient_id = Column(Integer, ForeignKey("patients.id", ondelete="CASCADE"), nullable=False)
     admitted_by = Column(Integer, ForeignKey("nurses.id", ondelete="SET NULL"))  # Nurse who admitted
-    admission_date = Column(DateTime, default=datetime.utcnow)
+    admission_date = Column(DateTime, default=func.now())
     discharge_date = Column(DateTime, nullable=True)
     status = Column(Enum(ICUStatus,name="icu_status",), default=ICUStatus.ADMITTED)
 
