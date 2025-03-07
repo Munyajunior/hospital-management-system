7396bd37633f582c1f8d64dce134e0a22b03876b
munyajunior <ivojunior671@gmail.com>
2025-03-07 10:29:52 +0100

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from schemas.auth import UserCreate, UserResponse, Token, LoginRequest
