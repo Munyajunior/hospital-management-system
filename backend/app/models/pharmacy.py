1d7fcde5ca0421531536a81976bc2a69b642b9f7
munyajunior <ivojunior671@gmail.com>
2025-03-07 10:29:32 +0100

    Update pharmacy.py
    created model for inventory, manage pharmacy inventory

--- backend/app/models/pharmacy.py
+++ backend/app/models/pharmacy.py
@@ -12,14 +12,31 @@ class Prescription(Base):
     __tablename__ = "prescriptions"
     id = Column(Integer, primary_key=True, index=True)
     patient_id = Column(Integer, ForeignKey("patients.id", ondelete="CASCADE"), nullable=False)
-    prescribed_by = Column(Integer, ForeignKey("users.id"), nullable=False)  # Doctor who prescribed
-    drug_name = Column(String, nullable=False)
+    prescribed_by = Column(Integer, ForeignKey("users.id"), nullable=False)  
+    drug_name = Column(String, ForeignKey("inventory.drug_name"), nullable=False)
     dosage = Column(String, nullable=False)
     instructions = Column(Text, nullable=False)
-    status = Column(Enum(PrescriptionStatus, name="prescription_status"), default=PrescriptionStatus.PENDING, nullable=False)  
+    status = Column(Enum(PrescriptionStatus, name="prescription_status"), default=PrescriptionStatus.PENDING, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
-    dispensed_at = Column(DateTime, onupdate=func.now(),server_default=func.now())
-    
+    dispensed_at = Column(DateTime, onupdate=func.now(), server_default=func.now())
+
     # Relationships
     patient = relationship("Patient", back_populates="prescriptions")
     doctor = relationship("User", foreign_keys=[prescribed_by])
+    inventory = relationship("Inventory", back_populates="prescriptions")
+
+
+class Inventory(Base):
+
+    __tablename__ = "inventory"
+
+    id = Column(Integer, primary_key=True, index=True)
+    added_by = Column(Integer, ForeignKey("users.id"), nullable=False)
+    drug_name = Column(String, nullable=False, unique=True)
+    quantity = Column(Integer, nullable=False)
+    added_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now(), server_default=func.now())
+
+    # Relationship with prescriptions
+    prescriptions = relationship("Prescription", back_populates="inventory")
+    #pharmacist = relationship("User", foreign_keys=[added_by])
