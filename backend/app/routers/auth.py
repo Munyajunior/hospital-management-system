33743a56fbe853c00718c95c32caaef99de56648
munyajunior <ivojunior671@gmail.com>
2025-03-13 10:21:24 +0100

    Update auth.py
    created all uers router and delete user router

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from typing import List
 from datetime import timedelta
-from schemas.auth import UserCreate, UserResponse, Token, LoginRequest
+from schemas.auth import UserCreate, UserResponse, Token, LoginRequest, AllUserResponse
 from models.user import User
 from models.patient import Patient
 from utils.security import hash_password, verify_password, create_access_token
@@ -54,3 +55,20 @@ def login_user(login_cred: LoginRequest, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     access_token = create_access_token({"sub": user.id, "role":user.role}, expires_delta=timedelta(minutes=30))
     return {"access_token": access_token, "token_type": "bearer", "role": user.role, "sub":user.id}
+
+
+@router.get("/users", response_model=List[AllUserResponse])
+def all_users(db: Session = Depends(get_db), _: User = Depends(admin_only)):
+    all_users = db.query(User).all()
+    if not all_users:
+        return []
+    return all_users
+
+
+@router.delete("/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def all_users(user_id: int, db: Session = Depends(get_db), _: User = Depends(admin_only)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(user)
+    db.commit()
